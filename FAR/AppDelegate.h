e027d8ddf9b236a541f91a1488d59e5dc674bf42
MO TAO <taomo@mos-imac.local>
2016-02-02 20:32:32 +0800

    Initial Commit

---
+++ FAR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FAR
+//
+//  Created by MO TAO on 2/2/2016.
+//  Copyright © 2016 MO TAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FAR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FAR
+//
+//  Created by MO TAO on 2/2/2016.
+//  Copyright © 2016 MO TAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
